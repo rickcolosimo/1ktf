dcca7fad6bf524045d8f270ec3ba3f587043c2e7
rickcolosimo <rick@thoughtstorm.com>
2013-11-12 21:16:30 -0500

    fan layout

--- app/controllers/fans_controller.rb
+++ app/controllers/fans_controller.rb
@@ -6,5 +6,10 @@ class FansController < ApplicationController
 def new
 end
 
+def create
+
+redirect_to fans_path # this sends me back to the Index page. Not sure that's what I want....
+end
+
 
 end
